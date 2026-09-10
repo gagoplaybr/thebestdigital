74d03b38314478ae1b2a53cd14363e57b991d331
gagoplaybr <gagoplaybr@gmail.com>
2026-09-10 01:04:16 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'the-best-pwa-v4';
+const CACHE = 'the-best-pwa-v5';
 const CORE = [
   '/', '/index.html', '/manifest.webmanifest',
   '/logo-the-best-digital.png', '/dashboard_powerbi_preview.png', '/video-poster.jpg',
