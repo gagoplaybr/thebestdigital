ed4f0a772eb7df53a9c3b9e26883f7af396e492f
gagoplaybr <gagoplaybr@gmail.com>
2026-09-12 17:43:42 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "the-best-marketplace-v8-1-3-botao-sobre-imagem";
+const CACHE_NAME = "the-best-marketplace-v8-1-4-card-premium-final";
 const APP_SHELL = [
   "/",
   "/index.html",
