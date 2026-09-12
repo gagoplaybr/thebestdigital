27e20de6a9a3400e23995740ff76bc4f35458c69
gagoplaybr <gagoplaybr@gmail.com>
2026-09-12 18:25:08 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "the-best-marketplace-v8-1-4-card-premium-final";
+const CACHE_NAME = "the-best-marketplace-v8-1-5-calendario-contas";
 const APP_SHELL = [
   "/",
   "/index.html",
